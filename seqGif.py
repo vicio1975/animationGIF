3dbf9e249c6fdda4ae5bfb7d5d1c8f64f756486b
Vicio75 <v.sammartano@gmail.com>
2023-07-24 09:54:44 +0200

    code update

--- seqGif.py
+++ seqGif.py
@@ -14,15 +14,15 @@ def main():
     location = os.getcwd()
     images = []
     try:
-        filenames = sorted((fn for fn in os.listdir(location) if fn.endswith('.jpg'))) # this iteration technique has no built in order, so sort the frames
+        filenames = sorted((fn for fn in os.listdir(location) if (fn.endswith('.jpeg') or fn.endswith('.jpg')))) # this iteration technique has no built in order, so sort the frames
         for filename in filenames:
-            images.append(imageio.imread(filename))
-        #or the faster way images = list(map(lambda filename: imageio.imread(filename), filenames))
-        imageio.mimsave(os.path.join('movie.gif'), images, duration=0.25, loop=0) # modify duration as needed
+            images.append(imageio.v2.imread(filename))
+        imageio.mimsave(os.path.join('movie.gif'), images, duration=0.1, loop=0) # modify duration as needed
     except Exception as e:
         raise e
         print("No image files in here!")
 
 if __name__ == "__main__":
     main()
+
 
